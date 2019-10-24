788af0aaa0200d7bd0427d4b80ce9b8181433af3
Ankur kaul <ankurkaul1894@gmail.com>
2019-10-23 20:22:27 -0700

    sign up options and signup email activity framework complete

---
+++ app/src/main/java/com/crazy4web/myapplication/ui/signup/SignUpEmail.java
@@ -0,0 +1,35 @@
+package com.crazy4web.myapplication.ui.signup;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.crazy4web.myapplication.R;
+
+public class SignUpEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_email);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/crazy4web/myapplication/ui/signup/SignUpOptions.java
@@ -0,0 +1,16 @@
+package com.crazy4web.myapplication.ui.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crazy4web.myapplication.R;
+
+public class SignUpOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_options);
+    }
+}
