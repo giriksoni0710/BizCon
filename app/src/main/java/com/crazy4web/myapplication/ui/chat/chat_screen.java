816b610dfa0d8ae39431adf24be71a15f4287adf
Girik soni <giriksoni0710@gmail.com>
2019-11-03 22:49:11 -0800

    User interface of the chat screen

---
+++ app/src/main/java/com/crazy4web/myapplication/ui/chat/chat_screen.java
@@ -0,0 +1,18 @@
+package com.crazy4web.myapplication.ui.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crazy4web.myapplication.R;
+
+public class chat_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+
+
+    }
+}
