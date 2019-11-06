4157cf65b640fcebf13276238f9255ea195330ba
Girik soni <giriksoni0710@gmail.com>
2019-11-05 17:54:08 -0800

    chat screen UI fix

--- app/src/main/java/com/crazy4web/myapplication/ui/chat/chat_screen.java
+++ app/src/main/java/com/crazy4web/myapplication/ui/chat/chat_screen.java
@@ -3,15 +3,20 @@ package com.crazy4web.myapplication.ui.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.crazy4web.myapplication.R;
 
 public class chat_screen extends AppCompatActivity {
 
+    EditText message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_screen);
+
+        message = findViewById(R.id.type_message);
+
 
 
     }
