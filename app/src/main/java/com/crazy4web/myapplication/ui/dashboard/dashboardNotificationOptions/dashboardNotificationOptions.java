05aa826a8641a45a2a5b581f96118caa3233dfb2
kaul <ankurkaul1894@gmail.com>
2019-11-05 19:11:59 -0800

    dashboard notification options done

---
+++ app/src/main/java/com/crazy4web/myapplication/ui/dashboard/dashboardNotificationOptions/dashboardNotificationOptions.java
@@ -0,0 +1,47 @@
+package com.crazy4web.myapplication.ui.dashboard.dashboardNotificationOptions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.crazy4web.myapplication.R;
+
+import java.util.List;
+
+public class dashboardNotificationOptions extends AppCompatActivity {
+
+    ListView listViewDashboard;
+    List<String> listItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_notification_options);
+//        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+//        View root = layoutInflater.inflate(R.layout.fragment_dashboard,  , false);
+
+//        listViewDashboard = findViewById(R.id.listViewDashboard);
+//        listItems.add("Inbox Messages");
+//        listItems.add("Who viewed your business");
+//        listItems.add("Shares");
+//
+//
+//        ArrayAdapter listAdapter = new ArrayAdapter(getApplicationContext() ,android.R.layout.simple_list_item_1, listItems);
+//        listViewDashboard.setAdapter(listAdapter);
+//
+//        listViewDashboard.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+////                Log.d(TAG, "tapped"+i);
+//                Toast.makeText(getApplicationContext(), "clicked item: "+i, Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+}
