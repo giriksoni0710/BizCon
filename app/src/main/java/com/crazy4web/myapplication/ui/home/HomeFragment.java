403bf35e91291bcb71527cb46af7174cc66b686b
Girik soni <giriksoni0710@gmail.com>
2019-11-06 01:20:10 -0800

    Single business connect

--- app/src/main/java/com/crazy4web/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/crazy4web/myapplication/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.crazy4web.myapplication.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +13,13 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.crazy4web.myapplication.R;
+import com.crazy4web.myapplication.ui.cardDetail.DetailActivityFragment;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
@@ -26,6 +29,9 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    CardView cardView;
+    Intent i;
 
     private int[] images = new int[]{
             R.drawable.ic_notifications_black_24dp,R.drawable.ic_dashboard_black_24dp, R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground};
@@ -38,6 +44,18 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        cardView = root.findViewById(R.id.business_card);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            i = new Intent(getContext(), DetailActivityFragment.class);
+
+            startActivity(i);
+
+            }
+        });
 
         // search
 
